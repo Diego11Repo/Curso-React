4d2188fb76ade94a1f5507ae5fc8e6a32e7b801d
Diego <diegoayanezvega@hotmail.com>
2020-06-30 09:55:47 -0500

    01-Primer ejercicio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const arreglo = [1, 2, 3, 4];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let arreglo2 = [...arreglo, 5];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(arreglo)
+console.log(arreglo2);
